5c93436e2b966aa08b2dff2f390756f6c0c87748
mansi7696 <100503990+mansi7696@users.noreply.github.com>
2022-02-27 22:17:52 +0530

    Update topsis.py

--- Topsis-101917203-Mansi/topsis.py
+++ Topsis-101917203-Mansi/topsis.py
@@ -1,51 +1,78 @@
+import pandas as pd
 import numpy as np
 
+def topsis(filename,weights,impacts):
 
-def topsis(a,weights,benefits):
+    #Step1 - Getting Decison Matrix
+    dataset = pd.read_csv(filename)
+    dataset = dataset.values[:,1:]
+    dm = pd.DataFrame(dataset).to_numpy(dtype = float)
+
+    #Step2 - Calculating Normalised Decison Matrix
+    numRows = len(dm)
+    numCols = len(dm[0])
+
+    #getting denominator
+    denom = []
+    for j in range(numCols):
+        sum = 0
+        for i in range(numRows):
+           sum = sum + (dm[i][j] ** 2)
+        value = sum ** 0.5
+        denom.append(value)
     
-    if(len(weights)!=np.size(a,1)):
-        raise Exception('number of weights doesn\'t match number of columns')
-    
-    if(len(benefits)!=np.size(a,1)):
-        raise Exception('number of benefits doesn\'t match number of columns')
-    
-        
-    b = np.zeros([np.size(a,0),0])
-    for column in a.T:
-        t = np.sqrt(sum(np.square(column)))
-        column = column/t
-        b = np.column_stack((b,column))
-    # b is normalized decision matrix
-        
-        
-    
-    # weighted normalized decision matrix
-    c=b*weights
-    
-    v = np.zeros([2,0])
-    #a.shape[1]= num of columns
-    
-    for i in range(c.shape[1]):
-        M = max(c.T[i])
-        m = min(c.T[i])
-        if(benefits[i]=='+'):
-            v=np.column_stack((v,[M,m]))
-        else:        
-            v=np.column_stack((v,[m,M]))
-                          
-    s = np.zeros([0,2])
-    #s[0]= s+ , s[1]=s-
-    for row in c:
-        sp = np.sqrt(np.sum(np.square(row-v[0])))
-        sn = np.sqrt(np.sum(np.square(row-v[1])))    
-        s = np.row_stack((s,[sp,sn]))
+    #updating matrix
+    for i in range(numRows):
+        for j in range(numCols):
+            dm[i][j] = dm[i][j] / denom[j]
     
     
-    p = np.zeros(0)
-    for row in s:
-        pi = (row[1]/(row[0]+row[1]))
-        p = np.append(p,pi)
-        
-        
-    result = np.where(p == np.amax(p))
-    return result[0]
+    #Step3 - Calculating weighted Normalised Matrix
+    for j in range(numCols):
+        for i in range(numRows):
+            dm[i][j] = dm[i][j] * float(weights[j])
+    
+    #Step4 - Calulating ideal best and worst for each column
+    maxx = np.amax(dm, axis=0)
+    minn = np.amin(dm, axis=0)
+    vbest = []
+    vworst = []
+    for i in range(numCols):
+        if impacts[i] == 1:
+            vbest.append(maxx[i])
+            vworst.append(minn[i])
+        elif impacts[i] == 0:
+            vbest.append(minn[i])
+            vworst.append(maxx[i])
+    
+    #Step5(a) - Calculating Eucledian Distance from ideal(best)
+    dbest = []
+    for i in range(numRows):
+        sum1 = 0
+        for j in range(numCols):
+            sum1 = sum1 + ((dm[i][j] - vbest[j]) ** 2)
+        dist1 = sum1 ** 0.5
+        dbest.append(dist1)
+            
+    #Step5(b) - Calculating Eucledian Distance from ideal(worst)
+    dworst = []
+    for i in range(numRows):
+        sum2 = 0
+        for j in range(numCols):
+            sum2 = sum2 + ((dm[i][j] - vworst[j]) ** 2)
+        dist2 = sum2 ** 0.5
+        dworst.append(dist2)
+    
+    #Step6 - Calculating Performance
+    perf = []
+    for i in range(numRows):
+        p = dworst[i] / (dworst[i] + dbest[i])
+        perf.append(round(p,7))
+    
+    #Result - Printing rank and best model
+    rank = perf.copy()
+    rank.sort(reverse=True)
+    
+    print("Index       Score       Rank")
+    for i in range(numRows):
+        print(str(i+1)+"       "+str(perf[i])+"       "+str(rank.index(perf[i]) + 1))
